a076cadbab512d7851e93a5a9079a7e2b0974e7f
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-25 08:55:12 +0000

    CFB19.5

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace aula19._5
+{
+    internal class Program
+    {
+        static void Main(){
+
+            int[] num = new int[10];
+
+            for(int i = 0; i < num.Length; i++)
+            {
+                num[i] = 0;
+            }
+
+            for(int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine("Valor de num na pos {0}: {1}", i, num[i]);
+            }
+        }
+    }
+}
